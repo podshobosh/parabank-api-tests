aa12e357523ac028f98e8055cf0ff9cdaedb3b50
podshobosh <podsho1704@gmail.com>
2026-08-27 13:33:38 -0400

    first commit

---
+++ src/test/java/com/podsho/parabank/runners/TestRunner.java
@@ -0,0 +1,20 @@
+package com.podsho.parabank.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "classpath:features",
+    glue = "com.podsho.parabank.stepdefinitions",
+    plugin = {"pretty", 
+              "html:target/cucumber-reports.html",
+              "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+    monochrome = true,
+    dryRun = false          
+
+
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests{
+    
+}
