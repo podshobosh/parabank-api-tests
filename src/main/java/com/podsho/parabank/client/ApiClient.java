0a3233e64a71564751c7f1f2a1850be68228aef2
Podsho <podsho1704@gmail.com>
2026-08-27 22:49:29 -0400

    implemented Creating Account feature

--- src/main/java/com/podsho/parabank/client/ApiClient.java
+++ src/main/java/com/podsho/parabank/client/ApiClient.java
@@ -1,5 +1,7 @@
 package com.podsho.parabank.client;
 
+import java.util.Map;
+
 import com.podsho.parabank.utils.ConfigReader;
 
 import static io.restassured.RestAssured.given;
@@ -7,37 +9,48 @@ import io.restassured.response.Response;
 
 public class ApiClient {
 
-
     // final base URL shared through all the calls
     private static final String BASE_URL = ConfigReader.getProperty("base.url");
 
     // GET response : takes in given endpoint
     public static Response get(String endpoint) {
         return given()
+                .accept("application/json")
                 .when()
                 .get(BASE_URL + endpoint);
     }
 
     public static Response post(String endpoint, Object requestBody) {
         return given()
+                .accept("application/json")
                 .contentType("application/json")
                 .body(requestBody)
                 .when()
                 .post(BASE_URL + endpoint);
     }
 
-    public static Response put(String endpoint, Object requestBody){
+    public static Response put(String endpoint, Object requestBody) {
         return given()
-        .contentType("application/json")
-        .body(requestBody)
-        .when()
-        .put(BASE_URL + endpoint);
+                .accept("application/json")
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .put(BASE_URL + endpoint);
     }
 
-    public static Response delete(String endpoint){
+    public static Response delete(String endpoint) {
         return given()
+                .accept("application/json")
                 .when()
                 .delete(BASE_URL + endpoint);
+    }
+
+    public static Response postWithParams(String endpoint, Map<String, Object> queryParams) {
+        return given()
+                .accept("application/json")
+                .queryParams(queryParams)
+                .when()
+                .post(BASE_URL + endpoint);
     }
 
 }
